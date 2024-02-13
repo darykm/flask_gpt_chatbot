93a5ce45f35271b9d6ec0a4499a7d5d1f9588fce
daryk <darykm@gmail.com>
2024-02-13 13:37:15 -0800

    first commit of project

--- main.py
+++ main.py
@@ -16,21 +16,22 @@ def index():
 @app.route('/gpt4', methods=['GET', 'POST'])
 def gpt4():
     user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    content = chat_completion.choices[0].message["content"]
 
 
-    try:
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": user_input
-                }
-            ],
-            model="gpt-3.5-turbo",
-        )
-        content = chat_completion.choices[0].message["content"]
-    except openai.RateLimitError:
-        content = "The server is experiencing a high volume of requests. Please try again later."
+    # try:
+
+    # except openai.RateLimitError:
+    #     content = "The server is experiencing a high volume of requests. Please try again later."
 
     return jsonify(content=content)
 
